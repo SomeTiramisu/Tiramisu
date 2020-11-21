21b253234f00637665d614aad04d628de238ee79
a <a@a.a>
2020-11-22 00:22:05 +0100

    rewrite Book with back support and filename sort

--- book.cpp
+++ book.cpp
@@ -1,23 +1,34 @@
 #include "book.h"
 
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <QtCore>
+
 extern "C" {
 #include <archive.h>
 #include <archive_entry.h>
+#include "strnatcmp/strnatcmp.h"
 };
 
 Book::Book()
 {
-    archive = archive_read_new();
-    archive_read_support_filter_all(archive);
-    archive_read_support_format_zip(archive);
-    archive_read_support_format_rar(archive);
-    archive_read_open_filename(archive, "/storage/emulated/0/b.cbr", 10240);
+    openArchive("/storage/emulated/0/b.cbr");
 
+    int i = 0;
+    while (archive_read_next_header(archive, &entry) == ARCHIVE_OK) {
+        header h = {
+            .filename = std::string(archive_entry_pathname(entry)),
+            .index = i
+        };
+        headers.push_back(h);
+        archive_read_data_skip(archive);
+        i++;
+    }
+    archive_read_free(archive);
+    std::sort(headers.begin(), headers.end(), naturalCompare);
 
-    buf = new char[length];
-    archive_read_next_header(archive, &entry);
-    length = archive_entry_size(entry);
-    archive_read_data(archive, buf, length);
+    loadBufAt(cindex);
 
 
 }
@@ -25,27 +36,27 @@ Book::Book()
 Book::~Book()
 {
     delete[] buf;
-    archive_read_free(archive);
 }
 
 char* Book::getCurrent()
 {
+    int n = headers[cindex].index;
+    loadBufAt(n);
     return buf;
 }
 
 char* Book::getNext()
 {
-    archive_read_next_header(archive, &entry);
-    length = archive_entry_size(entry);
-    delete[] buf;
-    buf = new char[length];
-    archive_read_data(archive, buf, length);
+    int n = headers[++cindex].index;
+    qWarning("loading %s", headers[cindex].filename.c_str());
+    loadBufAt(n);
     return buf;
 }
 
 char* Book::getPrevious()
 {
-
+    int n = headers[--cindex].index;
+    loadBufAt(n);
     return buf;
 }
 
@@ -53,4 +64,32 @@ char* Book::getPrevious()
 unsigned int Book::getLength()
 {
     return length;
+}
+
+void Book::openArchive(std::string filename) {
+    archive = archive_read_new();
+    archive_read_support_filter_all(archive);
+    archive_read_support_format_zip(archive);
+    archive_read_support_format_rar(archive);
+    archive_read_open_filename(archive, filename.c_str(), 10240);
+}
+
+void Book::loadBufAt(int n) {
+    openArchive("/storage/emulated/0/b.cbr");
+    for (int i=0; i<=n; i++) {
+        archive_read_next_header(archive, &entry);
+    }
+    length = archive_entry_size(entry);
+    delete[] buf;
+    buf = new char[length];
+    archive_read_data(archive, buf, length);
+    archive_read_free(archive);
+}
+
+bool naturalCompare(const header &a, const header &b) {
+    int r = strnatcasecmp(a.filename.c_str(), b.filename.c_str());
+    if (r < 0) {
+        return true;
+    }
+    return false;
 }
--- book.h
+++ book.h
@@ -1,10 +1,17 @@
 #ifndef BOOK_H
 #define BOOK_H
+#include <vector>
+#include <string>
+
 extern "C" {
 #include <archive.h>
 #include <archive_entry.h>
 };
 
+struct header {
+    std::string filename;
+    int index;
+};
 
 class Book
 {
@@ -16,10 +23,16 @@ public:
     char* getPrevious();
     unsigned int getLength();
 private:
-    struct archive *archive;
-    struct archive_entry *entry;
-    char* buf;
+    archive *archive;
+    archive_entry *entry;
+    char* buf = new char[0];
     unsigned int length;
+    std::vector<header> headers;
+    void openArchive(std::string filename);
+    void loadBufAt(int n);
+    int cindex = 0;
 };
+
+bool naturalCompare(const header &a, const header &b);
 
 #endif // BOOK_H
