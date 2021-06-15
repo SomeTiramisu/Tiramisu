0d721e7c58207f6681d1552f71e1d8c4af3182c7
SomeTiramisu <SomeTiramisu@users.noreply.github.com>
2021-06-15 21:19:20 +0200

    define C API

--- src/libtiramisu/tiramisu.h
+++ src/libtiramisu/tiramisu.h
@@ -19,7 +19,30 @@ private:
     int m_bookSize{0};
     std::unique_ptr<PageScheduler> m_scheduler;
     std::unique_ptr<PagePreloader> m_preloader;
-    PageRequest m_req;
 };
+
+extern "C" {
+
+typedef
+struct {
+    Path filename;
+    std::unique_ptr<PageScheduler> scheduler;
+    std::unique_ptr<PagePreloader> preloader;
+} tiramisu;
+
+typedef
+struct {
+    int index;
+    cv::Mat img;
+} tiramisuPage;
+
+void tiramisu_open_filename(tiramisu* t, const char* fn);
+void tiramisu_get_page(tiramisu* t, tiramisuPage* p, int index, int width, int height);
+void tiramisu_page_get_data(tiramisuPage* p, char* buf, size_t length);
+size_t tiramisu_page_get_length(tiramisuPage* p);
+int tiramisu_page_get_index(tiramisuPage* p);
+int tiramisu_get_size(tiramisu* t);
+int tiramisu_get_progress(tiramisu* t);
+}
 
 #endif // TIRAMISU_H
